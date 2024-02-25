7ad9a714dcd3cae9b3c57936c6c44a2a4d2cdc3a
MWaqasAfridi <153609017+MWaqasAfridi@users.noreply.github.com>
2024-02-25 10:13:15 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,97 @@
+        // ASSINGMENT 1
+         //    Question 1
+        // alert("Hello Waqas");
+
+        // Question 2
+        // alert("Error!Please enter a valid password");
+
+        // Question 3
+        // alert("Welcome to JS Land...\n Happy Coding")
+
+        // Question 4
+        // alert("Welcome to JS Land...");
+        // alert("Happing Coding....\nKesy hu sb kairyat hai");
+
+        // Question 5
+        // console.log("hello world");
+
+        // Question no 6
+        // alert("Hello Waqas");
+        // alert("Error!Please enter a valid password");
+
+       
+
+    // Assignment 2
+    // Question 1
+    // var username;
+
+    // Question 2
+    // var myName="Muhammad Waqas";
+    // alert(myName);
+    // document.write(myName);
+    // console.log(myName);
+
+    // Question 3
+    // var message="Hello World";
+    // alert(message);
+
+    // Question 4
+    // var studentName="Muhammad Waqas";
+    // alert(studentName);
+
+    // var age="I am 22 years old";
+    // alert(age);
+
+    // var course="Certified Mobile Application Development";
+    // alert(course);
+
+    // Question 5
+    // var request="PIZZA\nPIZZ\nPIZ\nPI\nP";
+    // alert(request);
+
+    // Question 6
+    // var email="muhammadwaqas121960@gmail.com";
+    // alert("my email address is "+ email);
+
+    // Question 7
+    // var book="A Smarter way to learn Javascript";
+    // alert(book);
+
+    // Question 8
+    // document.write("Yah! I can write HTML content through Javascript");
+
+    // Question 9
+    // var a="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+    // alert(a);
+    // document.write(a);
+
+
+
+    // ASSIGNMENT 3
+    // Question 1
+    // var age="i am 23 years old";
+    // alert(age);
+
+    // Question 2
+    // var visitor;
+    // visitor="You have visited this site 78 times";
+    // alert(visitor);
+    // document.write(visitor);
+
+    // Question 3
+    // var birthYear="My birth year is 2002"
+    // document.write(birthYear);
+
+    // Question 4
+    // var name="Waqas khan";
+    // var product="T-shirt(s)";
+    // var quantity="3";
+    // document.write(name+" ordered "+quantity+" "+product+" on XYZ clothing store");
+    
+
+
+
+
+
+
+    
